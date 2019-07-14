4f13b68e9b16657700616b1b4b74984380876e81
Stepan Vasilyeu <38642038+vasiljeu95@users.noreply.github.com>
2019-07-14 18:03:11 +0300

    Initial Commit

---
+++ MyPlacesSwiftBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlacesSwiftBook
+//
+//  Created by Stepan Vasiljeu on 7/14/19.
+//  Copyright © 2019 Stepan Vasiljeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
